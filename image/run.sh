bba9f30671c96f150461a16a0e62171c4689ed72
venkat566 <venkatasai566@gmail.com>
2020-04-07 12:47:47 -0500

    updated dockerfile and run.sh

--- image/run.sh
+++ image/run.sh
@@ -13,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+systemctl start redis
+systemctl enable redis
 
 function launchmaster() {
   if [[ ! -e /redis-master-data ]]; then
